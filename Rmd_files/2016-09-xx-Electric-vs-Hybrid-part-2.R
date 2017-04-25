588a6e11549a4e8df6ea7d77cf9fa9168b42a2cd
Michael Ching <mikeching@yahoo.com>
2017-04-24 21:32:59 -1000

    add images and datasets

---
+++ Rmd_files/2016-09-xx-Electric-vs-Hybrid-part-2.R
@@ -0,0 +1,7 @@
+
+## Environmental Impact
+Ok, so the price is probably cheaper for the Prius, but what about the carbon cost? Let's assume I were getting the electricity from the electric company, which uses mostly oil and some coal to make the energy. They have utility (duh) scale generators that don't have to move, so I would assume that they are cleaner than a car's gas engine. 
+
+Coal generates, and oil 1.7 lbs per kwh and coal 2.07 lbs of carbon per kwh. Oahu has 71.1% oil and 19.1% coal
+
+I mean, the Leaf actually doesn't expend any carbon because our photovoltaic panels are generating the electricity, but there are some costs to manufacturing, transporting, and installing the panels.
